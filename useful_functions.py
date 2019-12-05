80b6947b76b968b12d1247c72356d83cdcd799c2
Leo <leo.guignard@gmail.com>
2019-12-05 16:50:14 +0100

    xml + python3 optimization

--- useful_functions.py
+++ useful_functions.py
@@ -19,10 +19,101 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-import os
+import os, sys
 import scipy.cluster.hierarchy as H
 from copy import deepcopy
 from scipy import ndimage as nd
+import xml.etree.ElementTree as ET
+if sys.version_info[0] < 3:
+    import cPickle as pkl
+else:
+    import pickle as pkl
+
+keydictionary = {'lineage': {'output_key': 'cell_lineage',
+                             'input_keys': ['lineage_tree', 'lin_tree', 'Lineage tree', 'cell_lineage']},
+                 'h_min': {'output_key': 'cell_h_min',
+                           'input_keys': ['cell_h_min', 'h_mins_information']},
+                 'volume': {'output_key': 'cell_volume',
+                            'input_keys': ['cell_volume', 'volumes_information', 'volumes information', 'vol']},
+                 'surface': {'output_key': 'cell_surface',
+                             'input_keys': ['cell_surface', 'cell surface']},
+                 'compactness': {'output_key': 'cell_compactness',
+                                 'input_keys': ['cell_compactness', 'Cell Compactness', 'compacity',
+                                                'cell_sphericity']},
+                 'sigma': {'output_key': 'cell_sigma',
+                           'input_keys': ['cell_sigma', 'sigmas_information', 'sigmas']},
+                 'label_in_time': {'output_key': 'cell_labels_in_time',
+                                   'input_keys': ['cell_labels_in_time', 'Cells labels in time', 'time_labels']},
+                 'barycenter': {'output_key': 'cell_barycenter',
+                                'input_keys': ['cell_barycenter', 'Barycenters', 'barycenters']},
+                 'fate': {'output_key': 'cell_fate',
+                          'input_keys': ['cell_fate', 'Fate']},
+                 'fate2': {'output_key': 'cell_fate_2',
+                           'input_keys': ['cell_fate_2', 'Fate2']},
+                 'fate3': {'output_key': 'cell_fate_3',
+                           'input_keys': ['cell_fate_3', 'Fate3']},
+                 'fate4': {'output_key': 'cell_fate_4',
+                           'input_keys': ['cell_fate_4', 'Fate4']},
+                 'all-cells': {'output_key': 'all_cells',
+                               'input_keys': ['all_cells', 'All Cells', 'All_Cells', 'all cells', 'tot_cells']},
+                 'principal-value': {'output_key': 'cell_principal_values',
+                                     'input_keys': ['cell_principal_values', 'Principal values']},
+                 'name': {'output_key': 'cell_name',
+                          'input_keys': ['cell_name', 'Names', 'names', 'cell_names']},
+                 'contact': {'output_key': 'cell_contact_surface',
+                             'input_keys': ['cell_contact_surface', 'cell_cell_contact_information']},
+                 'history': {'output_key': 'cell_history',
+                             'input_keys': ['cell_history', 'Cells history', 'cell_life', 'life']},
+                 'principal-vector': {'output_key': 'cell_principal_vectors',
+                                      'input_keys': ['cell_principal_vectors', 'Principal vectors']},
+                 'name-score': {'output_key': 'cell_naming_score',
+                                'input_keys': ['cell_naming_score', 'Scores', 'scores']},
+                 'problems': {'output_key': 'problematic_cells',
+                              'input_keys': ['problematic_cells']},
+                 'unknown': {'output_key': 'unknown_key',
+                             'input_keys': ['unknown_key']}}
+
+def _set_dictionary_value(root):
+    if len(root) == 0:
+        if root.text is None:
+            return None
+        else:
+            return eval(root.text)
+    else:
+        dictionary = {}
+        for child in root:
+            key = child.tag
+            if child.tag == 'cell':
+                key = int(child.attrib['cell-id'])
+            dictionary[key] = _set_dictionary_value(child)
+    return dictionary
+
+def xml2dict(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    dictionary = {}
+
+    for k, v in keydictionary.items():
+        if root.tag == v['output_key']:
+            dictionary[str(root.tag)] = _set_dictionary_value(root)
+            break
+    else:
+        for child in root:
+            value = _set_dictionary_value(child)
+            if value is not None:
+                dictionary[str(child.tag)] = value
+    return dictionary
+
+
+def read_property_file(path):
+    ext = path.split(os.path.extsep)[-1]
+    if ext == 'pkl':
+        with open(path, 'rb') as f:
+            properties = pkl.load(f, encoding='latin1')
+            f.close()
+    else:
+        properties = xml2dict(path)
+    return properties
 
 def get_ratio_mother_sisters_volume(mother, vals, lin_tree, inv_lin_tree, around=5):
     """ From a mother cell id (right before a division),
